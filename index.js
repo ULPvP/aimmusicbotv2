8352534ece39bbc0acfeda5d5f97aaa190c381a7
ULPvP <50048718+ULPvP@users.noreply.github.com>
2020-01-02 02:37:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.on('message', message => {
 		command.execute(message, args)
 	} catch (error) {
 		console.error(error);
-		message.reply('there was an error trying to execute that command!');
+		message.channel.send('there was an error trying to execute that command!');
 	}
 });
 
