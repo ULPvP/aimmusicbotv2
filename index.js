dbe06a55ac9fd70fd818312a5cabec47c7d3f2de
ULPvP <50048718+ULPvP@users.noreply.github.com>
2020-01-02 20:34:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ for (const file of commandFiles) {
 
 
 client.once('ready', () => console.log('READY!'));
-bot.user.setGame('創作者:UL老熊#1188|Aim!play')
+bot.user.setActivity('創作者:UL老熊#1188|Aim!play')
 
 client.on('message', message => {
 	if(msg.content.startsWith(prefix + help)) {
