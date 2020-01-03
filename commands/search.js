55dc261d96ec8e209858b42824ebfd6445250480
ULPvP <50048718+ULPvP@users.noreply.github.com>
2020-01-04 00:08:31 +0800

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -1,6 +1,7 @@
 const discord = require('discord.js');
 const client = new discord.Client();
-const search = require('youtube-search');
+
+const search = require（'yt-search'）;
 const opts = {
     maxResults: 25,
     key: process.env.YOUTUBE_API,
@@ -11,43 +12,64 @@ module.exports = {
 	name: 'search',
 	description: '搜尋指令',
 	cooldown: 1,
-	execute(message) {
+	execute(bot, message, args, ops) {
 
-    if(message.content.toLowerCase() === 'Aim!search') {
-        let embed = new discord.RichEmbed()
-            .setColor("#73ffdc")
-            .setDescription("請Aim!search 你要的歌名。記得最好打與歌曲最有效的關鍵字!")
-            .setTitle("YouTube Search API");
-        let embedMsg = await message.channel.send(embed);
-        let filter = m => m.author.id === message.author.id;
-        let query = await message.channel.awaitMessages(filter, { max: 1 });
-        let results = await search(query.first().content, opts).catch(err => console.log(err));
-        if(results) {
-            let youtubeResults = results.results;
-            let i  =0;
-            let titles = youtubeResults.map(result => {
-                i++;
-                return i + ") " + result.title;
-            });
-            console.log(titles);
-            message.channel.send({
-                embed: {
-                    title: '選出一個數字是你想要的歌曲',
-                    description: titles.join("\n")
-                }
-            }).catch(err => console.log(err));
-            
-            filter = m => (m.author.id === message.author.id) && m.content >= 1 && m.content <= youtubeResults.length;
-            let collected = await message.channel.awaitMessages(filter, { maxMatches: 1 });
-            let selected = youtubeResults[collected.first().content - 1];
+    
+ 
 
-            embed = new discord.RichEmbed()
-                .setTitle(`${selected.title}`)
-                .setURL(`${selected.link}`)
-                .setDescription(`${selected.description}`)
-                .setThumbnail(`${selected.thumbnails.default.url}`);
-
-            message.channel.send(embed);
+ 
+    //我們將使用提供的參數查找歌曲
+     search(args.join(' '), function (err, res) {
+ 
+        //如果出現問題。
+          if (err)return message.channel.send（“出了點問題”）；
+ 
+        //我們將只列出10首歌曲。
+        var videos = res.videos.slice（0，15）;
+ 
+        //在這裡，我們將響應。
+        var response ='';
+ 
+        //瀏覽列表中的每首歌曲，並在消息中堅持使用。
+        for (var i in videos) {
+ 
+            //用ID格式化消息。我們執行i + 1以確保第一首歌曲中沒有0。
+            // \ r \ n正在開始新行，並在行尾添加一個空格。
+            響應+ =`** [$ {parseInt（i）+ 1}]：** $ {videos [i] .title} \ r \ n`;
+ 
         }
-    }
-});
+ 
+         response +="選擇1-$ {videos.length}之間的數字。"；
+ 
+        //發送消息。
+        message.channel.send(response);
+ 
+        //設置過濾器，以檢查您是否在查詢列表中提供了介於0和指定數字之間的數字。
+           const filter = music => !isNaN(music.content) && music.content < videos.length + 1 && music.content > 0;
+
+ 
+        //使用該過濾器創建一個消息接收器。
+        const collection = message.channel.createMessageCollector（filter）;
+ 
+        //將我們在此處找到的所有視頻插入接收器。
+        collection.videos = videos；
+ 
+        //如果發送的消息中的數字介於0和指定的數字之間，我們將調用play命令。
+        collection.once('collect'，function(music){
+ 
+            //下載播放命令。
+            var commandFile = require('./ play.js');
+ 
+            //播放射擊並開始播放歌曲或添加到列表。
+            commandFile.run(bot, message, [this.videos[parseInt(music.content) -1].url], ops);
+ 
+        });
+ 
+    });
+ 
+}
+ 
+module.exports.help = {
+    name："搜索"
+    description："搜索歌曲"
+}
